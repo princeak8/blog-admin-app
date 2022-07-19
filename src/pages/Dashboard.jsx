96a57c00cdec58a313122f3840b6d3acfac72f3a
princeak <akalodave@gmail.com>
2022-07-19 11:22:18 +0100

    latest changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import AuthContext from "../context/AuthProvider";
 import dayjs from "dayjs";
 import styles from "./css/Dashboard.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, NavLink } from "react-router-dom";
 
 import { Capitalize } from "../utils/helpers";
 import * as ImIcon from "react-icons/im";
@@ -12,10 +12,24 @@ const Dashboard = () => {
   const authCtx = useContext(AuthContext);
   const isLoading = useSelector((state) => state.userDisplay.isLoading);
   const dispatch = useDispatch();
+  const posts = useSelector((state) => state.postsDisplay.allPosts);
 
   return (
     <div className={styles["container-fluid"]}>
-      <h1>Dashboard</h1>
+        <h1>Dashboard</h1>
+
+        <div style={{display:'flex', width: '80%', flexDirection: 'row'}}>
+              <Link to="/admin/posts" style={{width: '40%', height: 220, margin: 20, border: 'solid thin #000', backgroundColor: 'grey', position: 'relative'}}>
+                  <h1 style={{textAlign:'center', fontSize: 48}}>POSTS</h1>
+                  <div style={{position: 'absolute', bottom: 0, left:0}}>
+                      <b>PUBLISHED:</b>{posts.length} | <b>UNPUBLISHED:</b>0
+                  </div>
+              </Link>
+            <div style={{width: '40%', height: 220, margin: 20, border: 'solid thin #000'}}>
+
+            </div>
+        </div>
+
     </div>
   );
 };
